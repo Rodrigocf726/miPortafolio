7938852d490f0184551315895f5d04f9500f4886
Rodrigo Castro <Rodrigocf726@gmail.com>
2023-03-07 21:57:38 -0600

    cambios a enlaces

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 const burguer = document.querySelector('.burguer');
 const navegacion = document.querySelector('.menu-navegacion');
+const enlaces = document.querySelectorAll('.navegacion a');
 const close = document.querySelector('.close');
+
+
+enlaces.forEach( enlace => enlace.addEventListener('click', scrollLento));
 
 burguer.addEventListener( 'click', openMenu);
 close.addEventListener( 'click', closeMenu);
-navegacion.addEventListener( 'click', closeMenu);
+// navegacion.addEventListener( 'click', closeMenu);
+
+function scrollLento(e){
+  e.preventDefault();
+
+  const href = this.getAttribute("href");
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+  });
+
+}
 
 function openMenu(){
   navegacion.style.display = 'block';
